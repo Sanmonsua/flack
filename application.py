2b2b98b11c9b7a5ea71a0de139ececed46573fbe
Sanmonsua <sanmonsua@gmail.com>
2020-06-22 01:49:52 -0500

    Add sigin feature

--- application.py
+++ application.py
@@ -1,13 +1,29 @@
 import os
 
-from flask import Flask
+from flask import Flask, session, render_template, redirect, url_for, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.secret_key = 'asfsdshfsdjkh'
 socketio = SocketIO(app)
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return "Project 2: TODO"
+    if 'username' not in session:
+        if request.method == 'GET':
+            return redirect(url_for('signin'))
+        session['username'] = request.form.get('username')
+    return render_template('index.html', username=session['username'])
+
+
+@app.route("/sign-in")
+def signin():
+    return render_template('signin.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
