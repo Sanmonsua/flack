542ea06f6edd650db0426b47ed2448c8a332de04
Sanmonsua <sanmonsua@gmail.com>
2020-06-28 18:05:11 -0500

    Add : add channel functionality

--- static/index.js
+++ static/index.js
@@ -2,28 +2,57 @@ const msgSendTemplate = Handlebars.compile(document.querySelector('#msg_send').i
 const msgReceiveTemplate = Handlebars.compile(document.querySelector('#msg_received').innerHTML);
 const channelTemplate = Handlebars.compile(document.querySelector('#channel').innerHTML);
 
-let channels = []
-let messages = []
 
 document.addEventListener('DOMContentLoaded', () =>{
-  let channel1 = {"name":"general", "creator":"asfdsfk"};
-  let channel2 = {"name":"abcd", "creator":"flack"};
-  channels.push(channel1);
-  channels.push(channel2);
 
-  channels.forEach( c => {
-    console.log(c);
-    const channel = channelTemplate(c);
-    document.querySelector("#channels").innerHTML += channel;
-  });
+  load_channels();
 
-  document.querySelectorAll(".channel").forEach(c => {
-    c.onclick = function() {
-      console.log(this.name)
-      document.querySelector("#channel-name").innerHTML = this.dataset.name;
-      return false;
-    }
-  });
+  document.querySelector('#addChannelForm').onsubmit = () =>{
+    const request = new XMLHttpRequest();
+    const name = document.querySelector('#inputAddChannel').value;
+    console.log(name);
+    request.open('post', '/add-channel');
 
-
+    const data = new FormData();
+    console.log(name);
+    data.append('name', name);
+    request.send(data);
+  };
 });
+
+function load_channels(){
+  const request = new XMLHttpRequest();
+  request.open('post', '/channels');
+
+  request.onload = () =>{
+    const channels = JSON.parse(request.responseText);
+    channels.forEach( c => {
+      console.log(c);
+      const channel = channelTemplate(c);
+      document.querySelector("#channels").innerHTML += channel;
+    });
+
+    document.querySelectorAll(".channel").forEach(c => {
+      c.onclick = function() {
+        console.log(this.name)
+        document.querySelector("#channel-name").innerHTML = this.dataset.name;
+        return false;
+      }
+    });
+  }
+
+  request.send();
+}
+
+function add_channel(){
+  const request = new XMLHttpRequest();
+  const name = document.querySelector('#inputAddChannel').value;
+  console.log(name);
+  request.open('post', '/add-channel');
+
+  const data = new FormData();
+  console.log(name);
+  data.append('name', name);
+  request.send(data);
+  return false;
+}
